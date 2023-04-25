dd5c3000e8e92655750d37c6a990bf2331780701
Dung Pham <phamd@tcd.ie>
2023-04-25 16:25:21 +0100

    Update approximation function: Exclude the 1XN case

--- kronecker.py
+++ kronecker.py
@@ -1,6 +1,7 @@
 # Files containing functions for Kronecker product decomposition
 import numpy as np
 import unittest
+import math
 
 # Permutation operator
 def permute(mat, A_dim1, A_dim2, B_dim1, B_dim2):
@@ -23,10 +24,10 @@ def kron_decompose(A, A_dim1, A_dim2, B_dim1, B_dim2):
 def kron_aprox(A):
     A_dim1, A_dim2 = A.shape
     min_obj = np.inf
-    for a in range(1, A_dim1 + 1):
+    for a in range(2, int(math.sqrt(A_dim1 + 1))):
         if A_dim1 % a == 0:
             c = A_dim1 // a
-            for b in range(1, A_dim2 + 1):
+            for b in range(2, int(math.sqrt(A_dim2 + 1))):
                 if A_dim2 % b == 0:
                     d = A_dim2 // b
                     C, D = kron_decompose(A, a, b, c, d)
