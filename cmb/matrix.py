3d3aa869c725495c623a43091228ae644d93f8f9
Dung Pham <phamd@tcd.ie>
2023-04-25 16:25:42 +0100

    Add cmb experiment

---
+++ cmb/matrix.py
@@ -0,0 +1,88 @@
+import sys
+import os
+
+import numpy as np
+from cmb.utils import calculate_matrix_A, find_neighbors, calc_D_size, calculate_N_from_level
+
+# Load normal matrix from file
+def load_matrix(aFile):
+    with open(aFile, "r") as f:
+        A = np.loadtxt(f, delimiter=",")
+        return A
+
+# Load values from file and create a diagonal matrix
+def load_diagonal_matrix(aFile):
+    with open(aFile, "r") as f:
+        temp = np.loadtxt(f,delimiter=",")
+        T = np.zeros((temp.shape[0], temp.shape[0]))
+        for i in range(0,temp.shape[0]):
+            T[i,i] = temp[i]
+        return T
+
+# Load values from file and create a diagonal matrix
+def load_diagonal_matrix_inverse(aFile):
+    with open(aFile, "r") as f:
+        temp = np.loadtxt(f,delimiter=",")
+        T = np.zeros((temp.shape[0], temp.shape[0]))
+        for i in range(0,temp.shape[0]):
+            T[i,i] = 1/temp[i]
+        return T
+
+# this is used in evaluation.py to load t.csv and then this is used to generate the diagonals
+# of the precision matrix C
+def load_array(aFile):
+    with open(aFile, "r") as f:
+       return np.loadtxt(f,delimiter=",")
+
+# Compute the mat-vec product Bs utilising the Kronecker structure of B
+def Bs(s, A, N):
+    S = np.reshape(s, (N, A.shape[1]), order='F')
+    BS = np.dot(S, np.transpose(A))
+    y = np.reshape(BS, (-1,), order='F')
+    return y
+    
+# Generate matrix matrix B as the Kronecker product of A and the identity matrix size N
+def generate_matrix_B(A, N):
+    return np.kron(A, np.eye(N))
+
+# Generate matrix C as the Kronecker product of two diagonal matrices (T and diagN)
+def generate_matrix_C(T, N):
+    diagN = np.eye(N)
+    return np.kron(T, diagN)
+
+# Generate matrix D
+def generate_matrix_D(lvl):
+    size = calculate_N_from_level(lvl)
+    (m,n) = calc_D_size(lvl)
+    res = np.zeros((size,size),dtype=int)
+
+    for i in range(0, size):
+        neighbors = find_neighbors(i, m, n) 
+        for pos in neighbors:
+            res[i][pos] = 1
+        
+        res[i][i] = -1 * len(neighbors)
+    return res
+
+# Generate matrix Q as Kronecker product of diagonal matrix P and D square
+def generate_matrix_Q(D, n):
+    N = D.shape[0]     
+    qsize = n * N
+    Q = np.zeros((qsize,qsize), dtype = int)
+    DSqure = D.dot(D)
+    for s in range(0, qsize, N):
+        Q[s : s + N, s : s + N] = DSqure
+    Q = Q
+    return Q
+
+# Generate matrix S_hat = A^TTAP^{-1}, assumming that P is a identity matrix
+def generate_matrix_S_hat(A, T):
+    A_trans = np.transpose(A)
+    return A_trans.dot(T.dot(A))
+
+# Generate matrix Y_hat = YTAP^{-1}, with Y = reshape(y,N,m)
+def generate_matrix_Y_hat(y, N, m, T, A):
+    Y = np.reshape(y,(N, m), order='F')
+
+    return Y.dot(T.dot(A))
+    
---
+++ cmb/utils.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import numpy as np
+import math
+
+V_PLANCK = [x * (10**9) for x in [30.0, 44.0, 70.0, 100.0, 143.0, 217.0, 353.0, 545.0, 857.0]]
+V_0 = V_PLANCK[3]
+PLANCK_H = 6.62607015e-34
+BOLTZMANN_K = 1.380649e-23
+K_S = -2.65
+K_D = 1.5
+K_FF = -2.14
+T1 = 18.1
+L = "left"
+R = "right"
+U = "up"
+D = "down"
+
+# Calculate conversion factor c(v) based on the formula:
+# c(v) = (e^psi − 1)^2/psi^2e^psi where psi = hv/k_BT1
+def calc_conversion_factor(v):
+    psi = (PLANCK_H) * v / (BOLTZMANN_K * T1)
+    e_psi = np.exp(psi)
+    #print(psi)
+    return ((e_psi - 1) ** 2) / (psi*psi * e_psi)
+
+# Calculate B(v) based on the formula:
+# B(v) = v/[exp(hv/kBT1) − 1]
+def B(v):
+    return v / (np.exp(PLANCK_H * v/(BOLTZMANN_K * T1)) - 1)
+
+# Calculate synchrotron value based on the formula:
+# a_s(v, v0) = c(v) * (v/ v0) ^ k_s 
+def calc_synchrotron(v):
+    #print(calc_conversion_factor(v))
+    return calc_conversion_factor(v) * ((v / V_0) ** K_S)
+
+# Calculate galatic dust value based on the formula:
+# a_d(v, v0) = c(v) * B(v) / B(v0) * (v / v0)^k_d
+def calc_galatic_dust(v):
+    return calc_conversion_factor(v) * B(v) / B(V_0) * ((v / V_0)**K_D)
+
+# Calculate free-free emission value based on the formula:
+# a_ff(v, v0) = c(v)* (v/ v0)^k_ff
+def calc_free_emission(v):
+    return calc_conversion_factor(v) * ((v / V_0) ** K_FF)
+
+# Calculate the mxn matrix A
+# The first column is all 1 (CMB source)
+# The n-1 other columns represent other sources: synchrotron, glatic dust, free-free emission
+def calculate_matrix_A(n, m):
+    A = np.zeros((n, m))
+    A[:, 0] = 1
+    A[:, 1] = [calc_synchrotron(x) for x in V_PLANCK]
+    A[:, 2] = [calc_galatic_dust(x) for x in V_PLANCK]
+    A[:, 3] = [calc_free_emission(x) for x in V_PLANCK]
+    return A
+
+def pretty_print_matrix(X):
+    m = X.shape[0]
+    n = X.shape[1]
+    for i in range(m):
+        for j in range(n-1):
+            print(X[i, j], end=',')
+        print(X[i, n-1])
+    return
+
+def find_neighbors(i, m, n):
+    """find all the neighbours of i
+    Parameters
+    ----------
+    i : int
+        the index of the point that we need to find its neighbours
+    m : int
+        the number of rows of original matrix (3*Nside)
+    n : int
+        the number of columns of original matrix (4*Nside)
+    """
+
+    # All the neighours of i
+    res = {
+            L: i - m,
+            R: i + m,
+            U: i - 1,
+            D: i + 1
+        }
+        
+    # Remove all the invalid neighbours
+    if i % m == 0: # The first row
+        res.pop(U)
+    elif (i % n) == (n - 1): #The last row
+        res.pop(D)
+    if i - m < 0: # The first column
+        res.pop(L)
+    elif i + m > n * m - 1: # The last column
+        res.pop(R)
+    return res.values()
+
+def calc_D_size(v):
+    base = 2**v
+    return 3*base, 4*base
+
+def calculate_N_from_level(lvl):
+    # N = 12 * N_side ^ 2 = 12 * (2*lvl)^2
+    return (4**lvl) * 12
